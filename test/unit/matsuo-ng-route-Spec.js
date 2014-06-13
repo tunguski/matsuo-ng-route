f1b6f10a3c39a8df4793c94b3aad1ee81e80fc41
marek <marek.romanowski@gmail.com>
2014-06-13 02:26:06 +0200

    More code coverage

--- test/unit/matsuo-ng-route-Spec.js
+++ test/unit/matsuo-ng-route-Spec.js
@@ -20,7 +20,7 @@ beforeEach(inject(function ($httpBackend, $rootScope, $compile, $route) {
 }));
 
 
-describe("Matsuo Routing", function () {
+describe("Matsuo Routing -", function () {
   it("It contains correct routing definitions", function () {
     expect(route).not.toBeNull();
     expect(route.routes).not.toBeNull();
@@ -28,5 +28,43 @@ describe("Matsuo Routing", function () {
     expect(route.routes['/:dir/:parent/:idParentEntity/:idView']).not.toBeNull();
     expect(route.routes['/:dir/:idView/:idEntity']).not.toBeNull();
     expect(route.routes['/:dir/:idView']).not.toBeNull();
+  });
+
+  describe("contextPathInterceptor", function() {
+    var contextPathInterceptor;
+    beforeEach(inject(function (_contextPathInterceptor_) {
+      contextPathInterceptor = _contextPathInterceptor_;
+    }));
+
+    it("work with absolute path", function () {
+      var config = { url: '/api/7' };
+      contextPathInterceptor(config);
+      expect(config.url).toBe('/api/7');
+    });
+
+    it("append base_app_location when needed", function () {
+      window.base_app_location = 'xxx'
+      var config = { url: 'api/7' };
+      contextPathInterceptor(config);
+      expect(config.url).toBe('xxx/api/7');
+    });
+  });
+
+  describe("unauthorizedInterceptor", function() {
+    var unauthorizedInterceptor;
+    beforeEach(inject(function (_unauthorizedInterceptor_) {
+      unauthorizedInterceptor = _unauthorizedInterceptor_;
+    }));
+
+    it("work", function () {
+      unauthorizedInterceptor.responseError({ status: 401, data: 'data' });
+      expect($rootScope.loggedIn).toBe(false);
+
+      unauthorizedInterceptor.responseError({ status: 403, data: 'data' });
+      // no possible test actually
+
+      unauthorizedInterceptor.responseError({ status: 500, data: 'data' });
+      // no possible test actually
+    });
   });
 });
